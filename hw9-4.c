95c3a1d159f856ea1e41f3625ce16b73d5165997
z525090-Malbec-Raphael <z525090@shibaura-it.ac.jp>
2025-12-04 10:03:06 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+    
+    printf("Enter R, G, B value\n");
+    
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+    
+    printf("The hex code is #%02X%02X%02X\n", r, g, b);
+    
+    return 0;
+}
 
