d4bd047f5bb50d3a3f6ed664863b0f71cbcdf5ae
z525090-Malbec-Raphael <z525090@shibaura-it.ac.jp>
2025-12-04 10:36:46 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int array[5];
+    int array[4];
     int i, j, temp;
     
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 4; i++) {
         scanf("%d", &array[i]);
     }
     
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             if (array[j] > array[j + 1]) {
                 temp = array[j];
                 array[j] = array[j + 1];
@@ -18,7 +18,7 @@ int main() {
         }
     }
 
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 4; i++) {
         if (i > 0) printf(" ");
         printf("%d", array[i]);
     }
