8bfa3f5425b5a65e4d620576d6e0deadedbe0017
z525090-Malbec-Raphael <z525090@shibaura-it.ac.jp>
2025-12-04 10:28:46 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main() {
+    int array[5];
+    int i, j, temp;
+    
+    printf("Enter 5 numbers:\n");
+    
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &array[i]);
+    }
+    
+    printf("Before sorting: ");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("After sorting: ");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
