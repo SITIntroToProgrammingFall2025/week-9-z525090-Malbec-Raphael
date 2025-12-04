d82a88d01998bcc5848f1528aa46fe92a09710fe
z525090-Malbec-Raphael <z525090@shibaura-it.ac.jp>
2025-12-04 09:42:25 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int array[3][3];
+    int flipped[3][3];
+    
+    printf("Enter 3 x 3 array\n");
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    
+    printf("You entered\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            flipped[i][j] = array[i][2 - j];
+        }
+    }
+    
+    printf("Output\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", flipped[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
 
